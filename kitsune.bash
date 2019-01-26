03efcf57f1ad3eb6be885a63651dda2c2c5b0a0d
Beta Faccion <betafcc@gmail.com>
2019-01-26 08:53:57 -0200

    Stumbled on elm-like model-view-update

--- kitsune.bash
+++ kitsune.bash
@@ -1,142 +1,137 @@
-#!/usr/bin/env bash
-# -- EXPORTS
-kitsune_prompt_command() {
-  PS1="$(kitsune_ps1)"
+# -- VIEW
+kitsune_view() {
+  printf %b \
+         "${kitsune_view_tag[${kitsune_model_tag[key]}]}" \
+         "${kitsune_view_path[${kitsune_model_path[key]}]}" \
+         "${kitsune_view_git[${kitsune_model_git[key]}]}" \
+         "${kitsune_view_arrow[${kitsune_model_arrow[key]}]}"
 }
-kitsune_ps3='\[\e[31m\]● \[\e[m\]'
-kitsune_ps2='\[\e[31m\]▐ \[\e[m\]'
-kitsune_ps4='▐ \[\e[1;33m\]${FUNCNAME[0]}:${LINENO}:\[\e[0m\]q '
 
-# -- CONFIGURATION
-declare -a kitsune_ps1_sections=('path' 'git' 'arrow')
-declare -a kistune_env_providers=('git')
-
-# -- TEMPLATES
-# ❯❱▐
-declare -A kitsune_template_path_tag=(
-  [${HOME}/Desktop]='<bold + white:【<cyan:今>】>'
-  [${HOME}]='<bold + white:【<yellow:家>】>'
-  [/]='<bold + white:【<red:本>】>'
+declare -A kitsune_view_tag=(
+  [${HOME}/Desktop]='<bold+white:【<cyan:今>】>'
+  [${HOME}]='<bold+white:【<yellow:家>】>'
+  [/]='<bold+white:【<red:本>】>'
 )
 
-declare -A kitsune_template_path=(
-  [no_untagged]='$tag'
-  [single_untagged]='$tag<bold:${sys[W]} >'
-  [multiple_untagged]='$tag<bold:$(yes ❯ | head -n $(($n_untagged-1)) | paste -sd "") ${sys[W]} >'
+declare -A kitsune_view_path=(
+  [no_untagged]=''
+  [single_untagged]='<bold:${kitsune_model_sys[W]} >'
+  [multi_untagged]='<bold:${kitsune_model_path[arrows]} ${kitsune_model_sys[W]} >'
 )
 
-declare -A kitsune_template_git=(
-  [modified]='<bold+red:❪${git[branch]}❫ >'
-  [staged]='<bold+red:❪${git[branch]}❫ >'
-  [untracked]='<bold+red:❪${git[branch]}❫ >'
-  [behind_ahead]='<bold+yellow:❪${git[branch]}❫ >'
-  [ok]='<bold+cyan:❪${git[branch]}❫ >'
+declare -A kitsune_view_git=(
+  [modified]='<bold+red:❪${kitsune_model_git[branch]}❫ >'
+  [staged]='<bold+red:❪${kitsune_model_git[branch]}❫ >'
+  [untracked]='<bold+red:❪${kitsune_model_git[branch]}❫ >'
+  [behind_ahead]='<bold+yellow:❪${kitsune_model_git[branch]}❫ >'
+  [ok]='<bold+cyan:❪${kitsune_model_git[branch]}❫ >'
   [not_repo]=''
 )
 
-declare -A kitsune_template_arrow=(
+declare -A kitsune_view_arrow=(
   [erroed_last]='<bold+red:❱ >'
   [has_jobs]='<bold+yellow:❱ >'
   [ok]='<bold:❱ >'
 )
 
-# for exansion with @P
-kitsune_j='\j'
-kitsune_W='\W'
 
-kitsune_ps1() {
-  local env section
-  local -A sys=(
-    [q]=$?
-    [j]="${kitsune_j@P}"
-    [W]="${kitsune_W@P}"
-    [PWD]="${PWD}"
-  )
-  # NOTE: how to share memory from subshells? Needed for parallelization
-  for env in "${kistune_env_providers[@]}"; do
-    local -A "${env}"
-    "kitsune_env_${env}"
-  done
-  for section in "${kitsune_ps1_sections[@]}"; do
-    "kitsune_section_${section}"
-  done
+# -- MODEL
+declare -A kitsune_model_sys=(
+  [q]=''
+  [j]=''
+  [W]=''
+  [PWD]=''
+)
+
+declare -A kitsune_model_tag=(
+  [key]='/'
+  [levels]=0
+)
+
+declare -A kitsune_model_path=(
+  [key]=no_untagged
+  [arrows]=''
+)
+
+declare -A kitsune_model_git=(
+  [key]=not_repo
+  [branch]=''
+)
+
+declare -A kitsune_model_arrow=(
+  [key]=ok
+)
+
+# -- UPDATE
+kitsune_update() {
+  kitsune_update_sys
+  kitsune_update_tag
+  kitsune_update_path
+  kitsune_update_git
+  kitsune_update_arrow
 }
 
-# -- ENV PROVIDERS
-kitsune_env_git() {
-  git[branch]=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
+kitsune_j='\j'
+kitsune_W='\W'
+kitsune_update_sys() {
+    kitsune_model_sys[q]=$?
+    kitsune_model_sys[j]="${kitsune_j@P}"
+    kitsune_model_sys[W]="${kitsune_W@P}"
+    kitsune_model_sys[PWD]="${PWD}"
+}
 
-  if [ -n "${git[branch]}" ]; then
+kitsune_update_tag() {
+  local dir="${kitsune_model_sys[PWD]}"
+  local levels=0
+
+  until [ ${kitsune_view_tag[${dir}]+x} ]; do
+    dir="${dir%/*}"
+    ((++levels))
+  done
+
+  kitsune_model_tag[key]="${dir}"
+  kitsune_model_tag[levels]="${levels}"
+}
+
+kitsune_update_path() {
+  case "${kitsune_model_tag[levels]}" in
+    0) kitsune_model_path[key]=no_untagged;;
+    1) kitsune_model_path[key]=single_untagged;;
+    *)
+      kitsune_model_path[key]=multi_untagged
+      kitsune_model_path[arrows]=$(for _ in $(seq ${kitsune_model_tag[levels]}); do printf ❯; done)
+      ;;
+  esac
+}
+
+kitsune_update_git() {
+  kitsune_model_git[branch]=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
+
+  if [ -n "${kitsune_model_git[branch]}" ]; then
     if [ ! "$(git diff --name-only --diff-filter=M 2> /dev/null | wc -l )" -eq "0" ]; then
-       git[state]=modified
+       kitsune_model_git[key]=modified
     elif [ ! "$(git diff --staged --name-only --diff-filter=AM 2> /dev/null | wc -l)" -eq "0" ]; then
-      git[state]=staged
+      kitsune_model_git[key]=staged
     elif [ ! "$(git ls-files --other --exclude-standard | wc -l)"  -eq "0" ]; then
-      git[state]=untracked
+      kitsune_model_git[key]=untracked
     else
-      local number_behind_ahead="$(git rev-list --count --left-right '@{upstream}...HEAD' 2>/dev/null)"
+      local number_behind_ahead
+      number_behind_ahead="$(git rev-list --count --left-right '@{upstream}...HEAD' 2>/dev/null)"
       if [ ! "0${number_behind_ahead#*	}" -eq 0 -o ! "0${number_behind_ahead%	*}" -eq 0 ]; then
-        git[state]=behind_ahead
+        kitsune_model_git[key]=behind_ahead
       else
-        git[state]=ok
+        kitsune_model_git[key]=ok
       fi
     fi
   else
-    git[state]=not_repo
+    kitsune_model_git[key]=not_repo
   fi
 }
 
-# -- RENDERERS
-kitsune_section_path() {
-  local tag path_case dir="${sys[PWD]}" n_untagged=0
-  until [ ${kitsune_template_path_tag[${dir}]+x} ]; do
-    dir="${dir%/*}"
-    ((++n_untagged))
-  done
-  tag="${kitsune_template_path_tag[${dir}]}"
-
-  case "${n_untagged}" in
-    0) path_case=no_untagged;;
-    1) path_case=single_untagged;;
-    *) path_case=multiple_untagged;;
+kitsune_update_arrow() {
+  case "${kitsune_model_sys[q]},${kitsune_model_sys[j]}" in
+    0,0) kitsune_model_arrow[key]=ok;;
+    0,*) kitsune_model_arrow[key]=has_jobs;;
+    *,*) kitsune_model_arrow[key]=erroed_last;;
   esac
-  printf '%b' "${kitsune_template_path[${path_case}]@P}"
 }
-
-kitsune_section_git() {
-  printf '%b' "${kitsune_template_git[${git[state]}]@P}"
-}
-
-kitsune_section_arrow() {
-  local state
-  case "${sys[q]},${sys[j]}" in
-    0,0) state=ok;;
-    0,*) state=has_jobs;;
-    *) state=erroed_last;;
-  esac
-  printf '%b' "${kitsune_template_arrow[${state}]@P}"
-}
-
-kitsune_preprocess() {
-  local template_name key
-  for template_name in "${@}"; do
-    local -n template_table="${template_name}"
-
-    for key in "${!template_table[@]}"; do
-      template_table[${key}]="$("${kitsune_clc}" --escape "${template_table[${key}]}")"
-    done
-  done
-}
-
-
-if [[ "${BASH_SOURCE[0]}" = "${0}" ]]; then
-  kitsune_ps1 | "$(dirname "$0")"/lib/clc/clc
-else
-  kitsune_src_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-  kitsune_clc="${kitsune_src_dir}/lib/clc/clc"
-  kitsune_preprocess "${!kitsune_template_@}"
-  case "${1}" in
-    -a|--activate) PROMPT_COMMAND="kitsune_prompt_command ; ${PROMPT_COMMAND}"
-      ;;
-  esac
-fi
